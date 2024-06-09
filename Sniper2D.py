10aa472943895e7c0b863e61cff12214050739bc
Panithecracker <97905110+Panithecracker@users.noreply.github.com>
2024-06-09 14:57:39 +0200

    Update Sniper2D.py
    Added air drag equations

--- Sniper2D.py
+++ Sniper2D.py
@@ -1,4 +1,3 @@
-
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,6 +5,22 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.image as mpimg
 import random
 import pygame
+
+#Rifle geometry parameters:
+L1 = 3 #right body length
+L2 = 1 #scope length
+h = 0.5 #body to scope length
+#Bullet motion parameters:
+v = 45 #rifle's speed
+g = 10 #constant gravitational force
+mu = 0.1 #air drag coefficient
+dt = 1/24 #time step size
+#Step sizes and other global variables:
+duration = 5 #duration of the bullet motion simulation in seconds
+dtheta = 0.1 #rotation step size in radians
+in_air = False #control variable for bullet state (True if in air False otherwise)
+rotating = False #control variable for rifle state (True if rotating False otherwise)
+ground = -10 #y coordinate of the ground points (based on rifles coordinate system it is negative)
 
 #Rifle object:
 class Rifle:
@@ -59,42 +74,35 @@ class Rifle:
         print("Scope length "+str(self.scope_length))
         print("Scope height "+str(self.scope_height))
         print("Shooting angle"+str(self.shooting_angle))
-#Parametric equations for the trajectory of the bullet:
-def GetX(t,v,theta,L1):
-    x = L1*math.cos(theta)+v*math.cos(theta)*t
-    return x
-def GetY(t,v,theta,L1,g):
-    y = L1*math.sin(theta)+v*math.sin(theta)*t+0.5*g*t**2
-    return y
-''' Implementation to animate only during the time it flies until hitting the ground at height y = -10
-def max_of_two_numbers(num1, num2):#return largest number from 2
-    if num1 > num2:
-        return num1
-    else:
-        return num2
 
-def FindFligthTime(R) : #find duration of bullet flight shot from current state of rifle object R
-    global v,g
-    angle = R.shooting_angle
-    flight_time = max_of_two_numbers((-v*math.sin(angle)+math.sqrt((v*math.sin(angle))**2-2*g*(R.body_length*math.sin(angle)+10)))/(2*R.body_length*math.sin(angle)),(-v*math.sin(angle)-math.sqrt((v*math.sin(angle))**2-2*g*(R.body_length*math.sin(angle)+10)))/(2*R.body_length*math.sin(angle)))
-    return flight_time
 '''
-
-#Rifle geometry parameters:
-L1 = 3 #right body length
-L2 = 1 #scope length
-h = 0.5 #body to scope length
-#Bullet motion parameters:
-v = 45 #rifle's speed
-g = -10 #constant gravitational force
-dt = 1/24 #time step size
-#Step sizes and other global variables:
-duration = 5 #duration of the bullet motion simulation in seconds
-dtheta = 0.1 #rotation step size in radians
-in_air = False #control variable for bullet state (True if in air False otherwise)
-rotating = False #control variable for rifle state (True if rotating False otherwise)
-ground = -10 #y coordinate of the ground points (based on rifles coordinate system it is negative)
-
+class Bullet:
+    global g
+    def __init__(self,p_x,p_y,v_x,v_y,d):
+        self.position = np.array([p_x, p_y])
+        self.velocity = np.array([v_x,v_y])
+        self.drag = d #drag coefficient
+        self.acceleration = np.array([0,g])-self.drag*self.velocity
+    #2 alternative ways of updating the bullet (using the 2nd order differential equations or using the exact solution)
+    def Update(self,delta_time): #1st way (more general but not exact solution)
+        self.position += self.velocity*delta_time
+        self.velocity += self.acceleration*delta_time
+        self.acceleration =  np.array([0,g])-self.drag*self.velocity
+    def
+    '''
+#Parametric equations for the trajectory of the bullet:
+def GetX(t,v,theta,L1,mu): #mu is the constant drag coefficient (positive)
+    if mu > 0: #some air drag
+        x = L1*math.cos(theta)+(v*math.cos(theta))/(mu)*(1-math.exp(-mu*t))
+        return x
+    x = L1*math.cos(theta)+v*math.cos(theta)*t #no air drag case
+    return x
+def GetY(t,v,theta,L1,g,mu):
+    if mu > 0: #some air drag
+        y = L1*math.sin(theta)+((g)/(mu**2)+(v*math.sin(theta))/(mu))*(1-math.exp(-mu*t))-(g)/(mu)*t
+        return y
+    y = L1*math.sin(theta)+v*math.sin(theta)*t-0.5*g*t**2
+    return y
 
 #Define a sniper rifle object and create the interactive game figure with one axis ("landscape scale" view axes)
 MyRifle = Rifle(L1,L2,h,0)
@@ -103,7 +111,7 @@ ax.axis([-50,50,-50,50]) #x,y range
 ax.set_aspect('equal')  #equal scaling
 ax.set_title("Landscape view")
 bullet, = ax.plot([],[],marker = 'o',color = 'red',markersize = 2)  #bullet line plot
-time_register = ax.text(0.05, 0.95, f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º', transform=ax.transAxes, fontsize=12, verticalalignment='top') #timer and angle counter text box
+register = ax.text(0.05, 0.95, f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º', transform=ax.transAxes, fontsize=12, verticalalignment='top') #timer and angle counter text box
 body, = ax.plot([],[],color = 'brown',linewidth = 2.5)
 scope, = ax.plot([],[],color = 'black',linewidth = 2)
 left_support, = ax.plot([],[],color = 'black',linewidth = 1)
@@ -120,7 +128,7 @@ inset_ax.set_ylim(-3.5, 3.5)  # Set the y limits for a closer look around the or
 #create a target point (center of cirlce)
 target_x = 30
 target_y = 10
-target, = ax.plot([target_x],[target_y],marker = ".",color = "blue",markersize = 15)
+target, = ax.plot([target_x],[target_y],marker = '.',markersize = 10)
 
 
 # Add the same line plots to the inset axes
@@ -148,13 +156,13 @@ def CheckCollision(t_x,t_y,b_x,b_y): #checks if the target and bullet are close 
 def ShootingAnimation(frame): #shooting animation update function
     global MyRifle,bullet,in_air,duration,target,target_x,target_y
     t = frame*dt #current time based on frame number (between 0 and duration/dt)
-    new_x = GetX(t,v,MyRifle.shooting_angle,L1) #compute the new x and y coordinates
-    new_y = GetY(t,v,MyRifle.shooting_angle,L1,g)
+    new_x = GetX(t,v,MyRifle.shooting_angle,L1,mu=0.3) #compute the new x and y coordinates
+    new_y = GetY(t,v,MyRifle.shooting_angle,L1,g,mu = 0.3)
     bullet.set_data([new_x],[new_y]) #update the data in the bullet plot
-    time_register.set_text(f'Time: {t:.1f} s')  # Update time
+    register.set_text(f'Time: {t:.1f} s')  # Update time
     if abs(new_y) > 50 or abs(new_x) > 50: #bullet out of the x,y limits so dont animate anymore
         ani.event_source.stop()  # Stop the animation since it hit the target before duration seconds
-        time_register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Display angle info again
+        register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Display angle info again
         in_air = False  # update bullet state too
     if CheckCollision(target_x,target_y,new_x,new_y):
         print("HIT!")
@@ -163,12 +171,12 @@ def ShootingAnimation(frame): #shooting animation update function
         target.set_data([target_x],[target_y])
         target.set_color((random.random(),random.random(),random.random()))
         ani.event_source.stop()  # Stop the animation since it hit the target before duration seconds
-        time_register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Display angle info again
+        register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Display angle info again
         in_air = False #update bullet state too
     if frame == math.ceil(duration/dt): #last frame so update bullet state
         in_air = False
-        time_register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Display angle info again
-    return bullet, time_register #return anything
+        register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Display angle info again
+    return bullet, register #return anything
 
 def on_key_press(event): #key_press event handler
     if isinstance(event, str): #the normal input type for an event handler is of type event but since matplot does not support controller events, I had to manually trigger a keyboard event after detecting a controller button event through pygame
@@ -185,7 +193,7 @@ def on_key_press(event): #key_press event handler
         MyRifle.DrawRifle(inset_body, inset_scope, inset_left_support, inset_right_support, inset_bipod_left,inset_bipod_right)
         laser_line.set_data([MyRifle.B[0] ,10*MyRifle.B[0]],[MyRifle.B[1] ,10*MyRifle.B[1]])
         plt.draw() #make changes appear in screen right after
-        time_register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Update time text and angle info
+        register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Update time text and angle info
         rotating = False
     elif key == 'down' and not in_air and not rotating:
         print("Down")
@@ -196,7 +204,7 @@ def on_key_press(event): #key_press event handler
         MyRifle.DrawRifle(inset_body, inset_scope, inset_left_support, inset_right_support, inset_bipod_left,inset_bipod_right)
         laser_line.set_data([MyRifle.B[0] ,10*MyRifle.B[0]],[MyRifle.B[1] ,10*MyRifle.B[1]])
         plt.draw()
-        time_register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Update time text and angle info
+        register.set_text(f'Angle : {math.degrees(MyRifle.shooting_angle):.1f}º')  # Update time text and angle info
         rotating = False
     elif key == ' ' and not in_air and not rotating:
         in_air = True
@@ -208,6 +216,7 @@ def on_key_press(event): #key_press event handler
     elif key == 'escape':
         #close figure
         plt.close()
+
 def on_controller_press(): #controller event handler (it handles the controller_button_press event by triggering a button_event_press on keyboard for matlplotlib )
     pygame.event.pump()  # Update Pygame's internal state
     if joystick.get_button(10):  # R1 button
@@ -230,4 +239,5 @@ timer = fig.canvas.new_timer(interval=0.1)  # every 100ms the timer will call th
 timer.add_callback(on_controller_press)
 timer.start()
 ##################
+
 plt.show() #make all changes visual
